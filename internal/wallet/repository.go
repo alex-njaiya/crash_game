9b8a23be7adb757068919e8a3004fdc3890d2f42
alex-njaiya <ngangaalex795@gmail.com>
2026-09-12 01:46:03 +0300

    updated the repository to make the context public using the function WithTx that is used in the tests and also the service layer

--- internal/wallet/repository.go
+++ internal/wallet/repository.go
@@ -16,6 +16,11 @@ func contextWithKey(ctx context.Context, tx pgx.Tx) context.Context {
 	return context.WithValue(ctx, txKey{}, tx)
 }
 
+
+func WithTx(ctx context.Context, tx pgx.Tx) context.Context {
+	return contextWithKey(ctx, tx)
+}
+
 // extract an active transaction from a context if exists
 func TxtContext(ctx context.Context) (pgx.Tx, bool) {
 	tx, ok := ctx.Value(txKey{}).(pgx.Tx)
@@ -27,20 +32,19 @@ type Repository interface {
 	CreateWallet(ctx context.Context, UserID uuid.UUID) (*Wallet, error)
 	GetBalance(ctx context.Context, walletID uuid.UUID) (int64, error)
 	InsertLedgerEntry(ctx context.Context, entry LedgerEntry) error
+	FindByIdempotencyKey(ctx context.Context, key string) (*LedgerEntry, error)
+	LockWallet(ctx context.Context, walletID uuid.UUID) error
 }
-
 
 type PostgresRepository struct {
 	pool *pgxpool.Pool
 }
-
 
 func NewPostgresRepository(pool *pgxpool.Pool) *PostgresRepository {
 	return &PostgresRepository{
 		pool: pool,
 	}
 }
-
 
 func (r *PostgresRepository) CreateWallet(ctx context.Context, UserID uuid.UUID) (*Wallet, error) {
 	wallet := new(Wallet)
@@ -49,7 +53,7 @@ func (r *PostgresRepository) CreateWallet(ctx context.Context, UserID uuid.UUID)
 
 	// check for transaction in ctx
 	if tx, ok := TxtContext(ctx); ok {
-		err := tx.QueryRow(ctx, query, UserID).Scan(&wallet.ID,&wallet.UserID,&wallet.Currency,&wallet.CreatedAt)
+		err := tx.QueryRow(ctx, query, UserID).Scan(&wallet.ID, &wallet.UserID, &wallet.Currency, &wallet.CreatedAt)
 
 		if err != nil {
 			return nil, err
@@ -58,9 +62,8 @@ func (r *PostgresRepository) CreateWallet(ctx context.Context, UserID uuid.UUID)
 		return wallet, nil
 	}
 
-
 	// fallback to the connection pool if no transaction
-	err := r.pool.QueryRow(ctx, query, UserID).Scan(&wallet.ID,&wallet.UserID,&wallet.Currency,&wallet.CreatedAt)
+	err := r.pool.QueryRow(ctx, query, UserID).Scan(&wallet.ID, &wallet.UserID, &wallet.Currency, &wallet.CreatedAt)
 
 	if err != nil {
 		return nil, err
@@ -68,7 +71,6 @@ func (r *PostgresRepository) CreateWallet(ctx context.Context, UserID uuid.UUID)
 
 	return wallet, nil
 }
-
 
 func (r *PostgresRepository) GetBalance(ctx context.Context, walletID uuid.UUID) (int64, error) {
 	var balance int64
@@ -102,13 +104,11 @@ func (r *PostgresRepository) GetBalance(ctx context.Context, walletID uuid.UUID)
 	return balance, nil
 }
 
-
 func (r *PostgresRepository) InsertLedgerEntry(ctx context.Context, entry LedgerEntry) error {
 	query := `INSERT INTO ledger_entries (id, wallet_id, amount, type, reference_id, idempotency_key, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7)`
 
-
 	if tx, ok := TxtContext(ctx); ok {
-		_, err := tx.Exec(ctx, query, 
+		_, err := tx.Exec(ctx, query,
 			entry.ID,
 			entry.WalletID,
 			entry.Amount,
@@ -126,8 +126,7 @@ func (r *PostgresRepository) InsertLedgerEntry(ctx context.Context, entry Ledger
 
 	}
 
-
-	_, err := r.pool.Exec(ctx, query, 
+	_, err := r.pool.Exec(ctx, query,
 		entry.ID,
 		entry.WalletID,
 		entry.Amount,
@@ -142,4 +141,45 @@ func (r *PostgresRepository) InsertLedgerEntry(ctx context.Context, entry Ledger
 	}
 
 	return nil
+}
+
+func (r *PostgresRepository) FindByIdempotencyKey(ctx context.Context, key string) (*LedgerEntry, error) {
+	ledger_entry := new(LedgerEntry)
+
+	query := `SELECT id, wallet_id, amount, type, reference_id, idempotency_key, created_at FROM ledger_entries WHERE idempotency_key = $1`
+
+	if tx, ok := TxtContext(ctx); ok {
+		err := tx.QueryRow(ctx, query, key).Scan(&ledger_entry.ID, &ledger_entry.WalletID, &ledger_entry.Amount, &ledger_entry.Type, &ledger_entry.ReferenceID, &ledger_entry.IdempotencyKey, &ledger_entry.CreatedAt)
+
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, pgx.ErrNoRows
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+	}
+	err := r.pool.QueryRow(ctx, query, key).Scan(&ledger_entry.ID, &ledger_entry.WalletID, &ledger_entry.Amount, &ledger_entry.Type, &ledger_entry.ReferenceID, &ledger_entry.IdempotencyKey, &ledger_entry.CreatedAt)
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ledger_entry, nil
+}
+
+
+func (r *PostgresRepository) LockWallet(ctx context.Context, walletID uuid.UUID) error {
+	if tx, ok := TxtContext(ctx); ok {
+		_, err := tx.Exec(ctx, `SELECT id FROM wallets WHERE id = $1 FOR UPDATE`, walletID)
+
+		return err
+	}
+
+	return errors.New("lock wallet requires an active transaction")
 }
